7b92249f966a1691c08670728908c3fe721be860
Serhii <luber10@ukr.net>
2021-09-06 07:44:05 +0300

    react way of the samurai1.0

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <header className='header'>
+          <img src="https://cdn.brandfolder.io/AB44IBCB/at/vvbksrfzcqs5tm4p25h499s/BG_logo.png"></img>
+        </header>
+
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+
+const Profile = () => {
+    return (
+      <div className='content'>
+          <div>
+          <img src='https://www.bgprod.com/bg-master/uploads/home_image_BG_50yrs_adj_2_1680x440.jpg'></img>
+          </div>
+
+          <div>
+          <img src='https://media-exp1.licdn.com/dms/image/C4E0BAQHnK_bV3K38vg/company-logo_200_200/0/1624649883846?e=2159024400&v=beta&t=OgZiZ-6rGxd_IljA8w9zAWw18B8GbkE0qilpmuKq5Xc'></img>
+          </div>
+
+          <div>
+            My post
+            <div>
+              New posts
+            </div>
+            <div>
+              <div>post 1</div>
+              <div>post 2</div>
+              <div>post 3</div>
+            </div>
+          </div>
+
+
+
+        </div>
+
+    );
+}
+
+export default Profile;
